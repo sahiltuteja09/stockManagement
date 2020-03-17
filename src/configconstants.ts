35711abc566097aeb0dbc44a27b86abf9983bd56
Sahil <sahil@Sahils-MacBook-Pro.local>
2020-03-17 20:40:05 +0530

    dev for app is done for now working on pwa

--- src/configconstants.ts
+++ src/configconstants.ts
@@ -1,6 +1,6 @@
 export class CoreConfigConstant {
-  static apiUrl = 'http://localhost/php-stockmanagement/api/v1/'; 
- //  static apiUrl = 'http://viaraaa.com/apis/api/v1/'; 
+ // static apiUrl = 'http://localhost/php-stockmanagement/api/v1/'; 
+   static apiUrl = 'http://viaraaa.com/apis/api/v1/'; 
   static appName = 'Emartos';
     static uploadedPath = 'http://viaraaa.com/apis/uploads/'; 
     static appID = 'stockmanagementcom';
