b2a00140c8819d28b534a062e176cffc90ff9334
Sahil <sahil@Sahils-MacBook-Pro.local>
2020-03-28 08:29:23 +0530

    no changes

--- src/configconstants.ts
+++ src/configconstants.ts
@@ -1,10 +1,12 @@
 export class CoreConfigConstant {
   static appName = 'Emartos';
- static apiUrl = 'http://localhost/php-stockmanagement/api/v1/'; 
- static uploadedPath = 'http://localhost/php-stockmanagement/uploads/'; 
+// static apiUrl = 'http://localhost/php-stockmanagement/api/v1/'; 
+ //static uploadedPath = 'http://localhost/php-stockmanagement/uploads/'; 
   //  static apiUrl = 'http://viaraaa.com/apis/api/v1/'; 
   //  static uploadedPath = 'http://viaraaa.com/apis/uploads/';
-    
+  
+      static apiUrl = 'https://emartos.com/apis/api/v1/'; 
+    static uploadedPath = 'https://emartos.com/apis/uploads/';
     static appID = 'stockmanagementcom';
     static token = '5d41402abc4b2a76b9719d911017c592';
     static salt = '@$PE@!';
